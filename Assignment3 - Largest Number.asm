53a931d234811ed574dcde45d6867e7978721e5a
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-05-11 05:27:23 +0530

    updated

--- Assignment3 - Largest Number.asm
+++ Assignment3 - Largest Number.asm
@@ -1,69 +1,70 @@
 section .data
-    array db 11h, 55h, 33h, 22h, 44h
-    msg1 db 10, 13h, 'Largest no in an array is '
-    len1 equ $-msg1
+	array db 11h, 55h, 33h, 22h,44h
+	msg1 db 10,13,"Largest no in an array is:"
+	len1 equ $-msg1
 
 section .bss
-    cnt resb 1
-    result resb 16
-
-    dispbuff resb 2
-    %macro print 2
-        mov eax, 4
-        mov ebx, 1
-        mov ecx, %1
-        mov edx, %2
-        int 80h
-    %endmacro
+	cnt resb 1
+	result resb 16
 
 section .text
-    global _start
+	global _start
+	_start:
+		mov byte[cnt],5
+		mov rsi,array
+		mov al,0
+	LP: cmp al,[rsi]
+		jg skip
+		xchg al ,[rsi]
+		skip: inc rsi
+		dec byte[cnt]
+		jnz LP
 
-_start:
-    mov byte[cnt], 5          ; display
-    mov rsi, array
-    mov al, 0
-  LP:
-    cmp al, [rsi]
-    jg skip
-    xchg al, [rsi]
-    skip: 
-      inc rsi
-      dec byte [cnt]
-      jnz LP
-    
-    mov rax, 1      ; Store string
-    mov rdi, 1
-    mov rsi, msg1
-    mov rdx, len1
-    syscall
-        
-    call display
-    
-    display:
-        mov rbx, rax
-        mov rdi, result
-        mov cx, 16
-        up1:
-            rol rbx, 04
-            mov al, bl
 
-            and al, 0fh
-            cmp al, 09h
-            jg add_37
-            add al, 30h
-            jmp skip
-    
-    add_37:
-        add al, 37h
-        skip2:
-            mov [rdi], al
-            inc rdi
-            dec cx
-            jnz up1
-        print result, 16
-        ret
-            
-    mov rax, 60      ; exit system call
-    mov rdi, 0
-    syscall
+	;display
+		mov Rax,1
+		mov Rdi,1
+		mov Rsi,msg1
+		mov Rdx,len1
+		syscall
+
+
+	;display al
+
+	call display
+
+	;exit system call
+		mov Rax ,60
+		mov Rdi,0
+		syscall
+
+
+	%macro dispmsg 2
+		mov Rax,1
+		mov Rdi,1
+		mov rsi,%1
+		mov rdx,%2
+		syscall
+	%endmacro
+	display:
+		mov rbx,rax                      ; store no in rbx
+		mov rdi,result                   ;point rdi to result variable 
+		mov cx,16                        ;load count of rotation in cl  
+
+		up1: 
+			rol rbx,04               ;rotate no of left by four bits
+			mov al,bl	         ; move lower byte in dl		
+			and al,0fh               ;get only LSB
+			cmp al,09h               ;compare with 39h
+			jg add_37                ;if greater than 39h skip add 37    
+			add al,30h                
+			jmp skip1                 ;else add 30     
+		add_37: 
+			add al,37h                 
+		skip1: 
+			mov [rdi],al             ;store ascii code in result variable
+			inc rdi                  ; point to next byte
+			dec cx                   ; decrement counter
+			jnz up1                  ; if not zero jump to repeat
+			dispmsg result,16        ;call to macro
+	ret
